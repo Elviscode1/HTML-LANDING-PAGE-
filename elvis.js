af13276f5e43e7066ad0324b3a337f553c2d8589
Oluwatosin99 <bimbola777@gmail.com>
2020-10-05 14:02:54 +0100

    I am done editing ,I am trying to pull now

--- elvis.js
+++ elvis.js
@@ -1,3 +1,13 @@
-function getfirsthreeletters(name){
-    return name.substring(0, 3)
-}
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault()
+
+    let myname = document.getElementById("fullname").value;
+    let myemail = document.getElementById("email").value;
+    let myhelp = document.getElementById("help").value;
+
+    let users = {
+        name:myname,
+        email:myemail,
+        help:myhelp,
+    }
+})
